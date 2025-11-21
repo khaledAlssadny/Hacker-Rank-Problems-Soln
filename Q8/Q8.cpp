3f4a65088e0ab97d532b0880497e51a1b7107cbf
KhaledAlssadny <83821534+khaledAlssadny@users.noreply.github.com>
2025-11-21 19:59:14 +0200

    Add files via upload

---
+++ Q8/Q8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+
+    int *arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+        // cout << " ";
+    }
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+    return 0;
+}
